15d7e19c800b82e6a4a9f3a6c11ad5def0fefa89
Felix Winkler <Felix-Winkler96@web.de>
2026-04-16 16:24:19 +0200

    added classes

---
+++ models/moveable-object.class.js
@@ -0,0 +1,13 @@
+class MoveableObject{
+  x = 120;
+  y = 400;
+  img = "./assets/2_character_pepe_/2_walk/W-21.png";
+
+  moveLeft() {
+    console.log("move left");
+  }
+
+  moveRight() {
+    console.log("move right");
+  }
+}
--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MoveableObject();
 
 function init() {
   canvas = document.getElementById("gameCanvas");
   ctx = canvas.getContext("2d");
-  character.src = "./assets/2_character_pepe/2_walk/W-21.png";
-  setTimeout(function () {
-    ctx.drawImage(character, 10, 0, 80, 120);
-  }, 1000);
+  console.log("my Character is", character);
 }
